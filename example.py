fafb7e045b50c305913e0d13bd89993a48830a44
Julius Ross <juliusross@gmail.com>
2023-11-07 08:54:06 -0600

    Julius attempt at including the pressure are well

--- example.py
+++ example.py
@@ -4,7 +4,13 @@ import scipy.interpolate as interpolate
 import scipy.optimize as optimize
 
 #open the file for reading
-rootgrp = netCDF4.Dataset("era40files/Feb1978_1200.nc", "r", format="NETCDF4")
+rootgrp = netCDF4.Dataset("era40files/Feb1978_1200_temp.nc", "r", format="NETCDF4")
+rootgrp_pressure = netCDF4.Dataset("era40files/Feb1978_1200_pressure.nc", "r", format="NETCDF4")
+
+#print(rootgrp.dimensions['latitude'].size)
+#print(rootgrp_pressure.dimensions['latitude'].size)
+
+
 
 #get some dimension sizes
 nlong = rootgrp.dimensions['longitude'].size
@@ -15,9 +21,14 @@ timeslice = 0
 
 #extract the temperature
 t = rootgrp.variables['t2m']
+p = rootgrp_pressure.variables['msl']
+
 
 #restrict to the selected timeslice as numpy array
 t = t[timeslice, : , :]
+p = p[timeslice, : , :]
+
+
 
 #get coordinates of grid points as numpy arrays
 long = rootgrp.variables['longitude'][:]
@@ -26,8 +37,10 @@ lat = rootgrp.variables['latitude'][:]
 #interpolator expects grid coordinates in increasing order
 #so we flip latitudes (first axis)
 t = numpy.flip(t, axis=0)
+p = numpy.flip(p, axis=0)
 lat = numpy.flip(lat, axis=0)
 
+#### Temperature
 #make a copy to contain the antipodal values
 t2 = 0.*t
 #rotate by 180 degrees [longitude is second index]
@@ -40,15 +53,34 @@ t2 = numpy.flip(t2, axis=0)
 #construct an interpolator
 f = interpolate.RegularGridInterpolator((lat[:], long[:]), t-t2)
 
-ft = interpolate.RegularGridInterpolator((lat[:], long[:]), t)
-ft2 = interpolate.RegularGridInterpolator((lat[:], long[:]), t2)
+## Pressure
+#make a copy to contain the antipodal values
+p2 = 0.*p
+#rotate by 180 degrees [longitude is second index]
+nlong_half = int(nlong/2)
+p2[: , :] = numpy.concatenate((p[:, nlong_half:],
+                               p[:, :nlong_half]), axis=1)
+#flip in the latitude direction
+p2 = numpy.flip(p2, axis=0)
+
+#construct an interpolator
+fp = interpolate.RegularGridInterpolator((lat[:], long[:]), p-p2)
+
+#ft = interpolate.RegularGridInterpolator((lat[:], long[:]), t)
+#ft2 = interpolate.RegularGridInterpolator((lat[:], long[:]), t2)
 
 #make a function for the square of the interpolator
 
 def fsq(x):
-    return f(x)**2
+    return (fp(x)**2)+1000*(f(x)**2)
 
 #find a zero of f with initial guess x0
 x0 = numpy.array([30.,100.])
 ret = optimize.basinhopping(fsq, x0, niter=200)
-print(ret.x, ret.fun)
+print(ret.x, ret.fun,ret.message)
+
+print(f(ret.x))
+print(fp(ret.x))
+
+
+
