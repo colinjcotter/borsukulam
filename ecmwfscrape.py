e49af192a3172eb22eb876b43a5ac5b1f5a75e73
Julius Ross <juliusross@gmail.com>
2023-11-30 11:47:12 -0600

    fix

--- ecmwfscrape.py
+++ ecmwfscrape.py
@@ -135,7 +135,7 @@ with open(bu_datafile, 'w') as file:
 
 s3websitedirectory = "s3://ponderonward-website/Borsuk-Ulam/"
 logger.info('Writing bu_datafile '+str(bu_datafile)+' to S3 bucket '+str(s3websitedirectory))
-subprocessoutput=subprocess.run(["aws s3 cp "+bu_datafile+' '+s3websitedirectory+bu_datafile], shell=True)
+subprocessoutput=subprocess.run(["aws s3 cp "+bu_datafile+' '+s3websitedirectory+'bu-latest-data.js'], shell=True)
 logger.info(subprocessoutput)
 
 s3bufilesdirectory = "s3://bursk-ulam-bufiles/"
