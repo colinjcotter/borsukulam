d66995d15052f4a790eee45c8a0ef9550232bda2
Julius Ross <juliusross@gmail.com>
2023-11-30 10:44:04 -0600

    token

--- website/config.js
+++ website/config.js
@@ -1,2 +1,2 @@
-const mapboxaccesstoken = 'pk.eyJ1IjoianVsaXVzcm9zcyIsImEiOiJjbHAwZHY4MmswN2s1MnFtcmtnaXpqMDJ0In0._ffXmhexSsJ5QYaxSOp8cQ'
+const mapboxaccesstoken = 'Put Mapbox token here'
 const budirectory = './'
