c2316d5d00c39c19b942b6e33f90ef86d3072bc1
Julius Ross <juliusross@gmail.com>
2023-11-30 11:12:10 -0600

    point to s3

--- website/config.js
+++ website/config.js
@@ -1,2 +1,2 @@
-const mapboxaccesstoken = 'Put Mapbox token here'
-const budirectory = './'
+const mapboxaccesstoken = 'pk.eyJ1IjoianVsaXVzcm9zcyIsImEiOiJjbHAwZHY4MmswN2s1MnFtcmtnaXpqMDJ0In0._ffXmhexSsJ5QYaxSOp8cQ'
+const budirectory = 'https://bursk-ulam-bufiles.s3.us-east-2.amazonaws.com/'
