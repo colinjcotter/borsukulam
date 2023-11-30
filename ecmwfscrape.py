197a9a0cc3f3f2f5e1b7dedfc084ccdea76ebb1e
Julius Ross <juliusross@gmail.com>
2023-11-29 20:25:15 -0600

    removing the storing of the intial guess since it is no longer needed

--- ecmwfscrape.py
+++ ecmwfscrape.py
@@ -5,9 +5,8 @@
 #
 # 1. Scrape most recent data from ecmwf at steps firststep and laststep
 # 2. Compute ulampoints at certain intervals (hardcoded)
-# 3. Store these ulampoints to be used as an initial guess for the next time this file is run
-# 4. Creates a javascript file that contains (a) some of the data from the ulampoints that are found within tolerance
-# (b) The temperature and pressure data at step 0h and at step 6h
+# 3. Creates a javascript file that contains (a) some of the data from the ulampoints that are found within tolerance
+# (b) The temperature and pressure data at step firststep and at step laststep
 # 5. Copy this javascript file to my Amazon S3 bucket
 #
 
@@ -21,7 +20,6 @@ import xarray as xr
 import findulam
 import numpy
 import json
-import pickle
 import subprocess
 import time
 import sys
@@ -29,14 +27,14 @@ import sys
 
 
 
-
+# Script tries to find ulampoints within this tolerance
+tolerance = 1e-12
 
 logger = logging.getLogger(__name__)
 
-def my_handler(type, value, tb):
-	logger.exception("Uncaught exception: {0}",format(str(value)))
-
-sys.excepthook = my_handler
+#def my_handler(type, value, tb):
+#	logger.exception("Uncaught exception: {0}",format(str(value)))
+#sys.excepthook = my_handler
 
 logger.setLevel(logging.INFO)
 
@@ -46,9 +44,6 @@ parser.add_argument('--laststep', dest='laststep', type=int, help='Laststep')
 parser.add_argument('--date', dest='date', type=int, help='date')
 parser.add_argument('--time', dest='time', type=int, help='time')
 args = parser.parse_args()
-
-
-print(args)
 
 # The following should be adapted for the local filesystem structure
 ulamlistsfolder = 'ulamlists/' 
@@ -83,56 +78,21 @@ logger.info("Opening data from ECMWF")
 #Todo: Ideally we log the output of this command
 ds = xr.open_dataset('data.grib2',engine='cfgrib')
 
-# Todo: Convert the data to float to save space
-
-# The following tries to load data from the last time this file was run
-# and attempts to find an ulampoint close to the start time at step 0h
-# to feed as the initial guess of the numerical algorithm
-
-
-# The following is hardcoded just for testing
-#initialguess = [9.51287637483811, 50.63705018119461]
-
-initialguess = [0,0]
-logger.info('Trying to get initial guess from previously stored data')
-try: 
-	ulamlist_initial = pickle.load(open(latest_ulamlist_filename, "rb"))
-	ulamlist__initial_cropped = [ulam for ulam in ulamlist_initial if ((ulam['fun']< 1e-12) and (ulam['ulamtime']<ds.time.data))]
-	if len(ulamlist__initial_cropped)>0:
-		initialguess = ulamlist_initial_cropped[len(-1)]['ulampoint']
-		logger.info('Got initial guess ulampoint:'+str(ulamlist_initial_cropped[len(-1)]['ulampoint'])+'at ulamtime:'+str(ulamlist_initial_cropped[len(-1)]['ulamtime']))
-	else: 
-		logger.warning('Unable to find any useful initial guess from previously stored data')
-except:
-	logger.warning('Unable to get load data from previous stored data')
-
 #
 # This is where the computation is made
 #
 logger.info('Finding ulampoints')
 start = time.time()
-ulamlist = findulam.compute_ulampoints_between_timesteps(ds,steps=steps,N=720,initialguess=initialguess,tolerance=1e-10)
+ulamlist = findulam.compute_ulampoints_between_timesteps(ds,steps=steps,N=720,tolerance=tolerance)
 logger.info('Finding ulampoints completed in '+str(time.time()-start)+'s')
-#
-# Write the ulam points to a file for the next time this script is run
-#
-logger.info('Writing pickle')
-# Write the ulamlist as pickle
-ulamlist_filename = ulamlistsfolder+str(ds.time.data)+'.pickle'
-with open(ulamlist_filename, 'wb') as handle:
-	pickle.dump(ulamlist, handle, protocol=pickle.HIGHEST_PROTOCOL)
-## copy it to latest (there is surely a better way here)
-with open(latest_ulamlist_filename , 'wb') as handle:
-	pickle.dump(ulamlist, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
 
-#
 # Write the javascript file that contains the required data
 # All we record here is the ulamstep (rounded to second) and the ulampoint
 # Also we throw away those ulampoints not within a tolerances of 1e-8
 
-ulamlist_cropped = [[numpy.timedelta64(ulam['ulamstep'], 's').astype(float),ulam['ulampoint']] for ulam in ulamlist if ulam['OptimizeResult_basinhopping'].fun<1e-8]
+ulamlist_cropped = [[numpy.timedelta64(ulam['ulamstep'], 's').astype(float),ulam['ulampoint']] for ulam in ulamlist if ulam['OptimizeResult_basinhopping'].fun<tolerance]
 
 logger.info('ulamlist length: '+ str(len(ulamlist))+ ' of which '+ str(len(ulamlist_cropped))+ ' were found within tolerance')
 
@@ -159,5 +119,5 @@ f.close
 
 logger.info('Moving bu.js file to S3 bucket')
 #Todo: Ideally we log the output of this command
-subprocess=subprocess.run(["aws s3 cp "+latest_bu_filename+" s3://ponderonward-website/bu_latest.js"], shell=True)
+subprocess=subprocess.run(["aws s3 cp "+latest_bu_filename+" s3://ponderonward-website/Borsuk-Ulam/bu.js"], shell=True)
 logger.info(subprocess)
