3441db5f78c84dadac0dcfbb7e50fbd4390b73cc
Julius Ross <juliusross@gmail.com>
2023-12-06 21:05:53 -0600

    debug options

--- website/config.js
+++ website/config.js
@@ -1,2 +1,4 @@
-const mapboxaccesstoken = 'pk.eyJ1IjoianVsaXVzcm9zcyIsImEiOiJjbHAwZHY4MmswN2s1MnFtcmtnaXpqMDJ0In0._ffXmhexSsJ5QYaxSOp8cQ'
-const budirectory = 'https://bursk-ulam-bufiles.s3.us-east-2.amazonaws.com/'
+const budirectory = 'https://d1aipxdgb0yxq7.cloudfront.net/'  // Directory for regularly updated bu files
+const fallbackbufile = 'https://s3.us-east-2.amazonaws.com/julius-ross.com/Borsuk-Ulam/bu-fallback.js.gz' // static fallback bu file that is known to work
+const debugmode = 0  // Set to 1 to allow manual moving of time
+const debug_skipmaps =1 // Set to 1 to skip loading the maps for speed
