177fa03575ab3d5e2b2e400b7d32aaa18b73c4dc
Julius Ross <juliusross@gmail.com>
2023-12-18 20:04:17 -0600

    fixing some version issue

--- ecmwfscrape.py
+++ ecmwfscrape.py
@@ -1,25 +1,25 @@
-#
-# Script to scrape ECMWF data, calculate ulam points, and create some javascript files
-# to be used in Borsuk-Ulam explorer website
-#
-# Typical Usage: 
-# python3 ecmwfscrape.py --firststep 0 --laststep 12 --date 0 --time 6 --N 720
-#
-# This will get create and save ulampoints every minute in a 12 hour period of the forecast starting today at 06:00UTC
-#
-# In more detail it does the following:
-#
-# 1. Scrape most recent data from ecmwf at given time and date
-# 2. Compute ulampoints at certain intervals between timestep firststep and timestep laststep
-# 3. Creates three javascript files.  One is bu-date-firststep-laststep.js that has all the ulam 
-#    points and the temp/pressure data at firsttime and lasttime.  The second just contains the ulam points.  The third is bu-latest-data.js that is smaller and just
-#    points to the first file.
-# 5. Copy these two javascript file to my Amazon S3 bucket
-#
+'''
+Script to scrape ECMWF data, calculate ulam points, and create some javascript files
+to be used with the Borsuk-Ulam explorer website
 
+Typical Usage: 
+python3 ecmwfscrape.py --firststep 0 --laststep 12 --date 0 --time 6 --N 720
+
+This will get create and save ulampoints every minute in a 12 hour period of the forecast starting today at 06:00UTC
+
+The script is capable of doing the following:
+
+1. Scrape data from ecmwf
+2. Compute ulampoints at certain intervals between timestep 'firststep' and timestep 'laststep'
+3. Creates three javascript files.  One is bu-date-firststep-laststep.js that has all the ulam 
+   points and the temp/pressure data at firsttime and lasttime.  The second just contains the ulam points.  The third is a pointer file bu-latest-data.js that points
+   to the first file.
+4. Copy these three javascript files to S3 amazon buckets
+
+'''
 
 import logging 
-import argparse
+import configargparse
 from ecmwf.opendata import Client
 import xarray as xr
 import findulam
@@ -30,38 +30,46 @@ import time
 import sys
 
 
-# Script tries to find ulampoints within this tolerance
-tolerance = 1e-10
-
 #def my_handler(type, value, tb):
 #	logger.exception("Uncaught exception: {0}",format(str(value)))
 #sys.excepthook = my_handler
 
-#
-# Parse User Inputs
-#
+# # Parse User Inputs
+p = configargparse.ArgParser(default_config_files=['ecmwfscrape.conf'],description=__doc__,formatter_class=configargparse.RawDescriptionHelpFormatter)
+p.add('-c', '--my-config', required=False, is_config_file=True, help='config file path')
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--firststep', dest='firststep', type=int, help='Firststep',default=0)
-parser.add_argument('--laststep', dest='laststep', type=int, help='Laststep',default=6)
-parser.add_argument('--date', dest='date', type=int, help='date',default =0)
-parser.add_argument('--time', dest='time', type=int, help='time',default =0)
-parser.add_argument('--scrapedryrun', dest='scrapedryrun', type=int, default=0, help='if nonzero then do not download from ECMWF but assume data.grib2 is already there (for testing only)')
-parser.add_argument('--bu-directory',dest='bu_local_directory',type=str,default = "./website/bufiles/",help='directory to store js files produced from this script')
-parser.add_argument('--N',dest='N', type=int,default=720,help='Number of ulampoints to compute (default 720 which is 1 per minute for 12 hours')
-parser.add_argument('--s3dryrun',dest='s3dryrun',type=int, default=0, help='if nonzero then do not try to move files to s3 buckets but assume data.grib2 is already there (for testing only)')
-parser.add_argument('--logfile',dest='logfile',type=str, default = 'ecmwfscrape.log',help='logfile to use')
-#parser.add_argument('--verbose',dest='verbose',type=int, default=0,help='if non zero then verbose logging about the numerical method')
-args = parser.parse_args()
+p.add('--firststep',  type=int, help='Firststep',default=0) # this will be redone when findulam is updated
+p.add('--laststep', type=int, help='Laststep',default=6) # this will be redone when findulam is updated
+
+p.add('--grib2',type=str,help='skip downloading from ecmwf and use this grib2 file instead')
+p.add('--grib2_filename',default='data.grib2',type=str,help='grib filename to use when scraping from ecmwf')
+
+p.add('--date',type=int, help='date of the ecmwf scrape (ignored if --grib2 is specified)',default =0)
+p.add('--time', type=int, help='time of the ecmwf scrape (ignored if --grib2 is specified)',default =0)
+
+p.add('--N', type=int,default=720,help='Number of ulampoints to compute (default 720 which is 1 per minute for 12 hours')
+p.add('--tolerance',type=float,default = 1e-10,help='Tolerance for the ulam points (all that do not meet this are rejected)') 
+
+p.add('--bu-local-directory',default = "./", type=str,help='local directory to store bu data files produced from this script')
+p.add('--bufile_s3bucket',type=str,help='S3 bucket that bufile will be copied to')
+p.add('--bufile_url',type=str,help='Url corresponding to --bufile_s3bucket')
+p.add('--bupointer_s3bucket',type=str,help='S3 bucket that the pointer file will be copied to')
+
+p.add('--logfile',type=str, default = 'ecmwfscrape.log',help='logfile to use')
+p.add('--s3dryrun',type=int, default=0, help='if nonzero then do not copy files to s3 buckets (for testing only)') #improve the notation of this one
+p.add('--scrapedryrun', type=int, default=0, help='if nonzero then do not download from ECMWF but assume data.grib2 is already there (for testing only)') #to be removed
+p.add('--verbose',type=int, default=0,help='if non zero then verbose logging about the numerical method')
+args= p.parse_args()
 
 #
 # Setup logger
 #
 logging.basicConfig(filename = args.logfile,format='%(asctime)s %(levelname)s  %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-
-
+ConsoleOutputHandler = logging.StreamHandler()
+ConsoleOutputHandler.setLevel(logging.DEBUG)
+logger.addHandler(ConsoleOutputHandler)
+logger.setLevel(logging.DEBUG)
 
 # Initialize Data
 bu_local_directory  = args.bu_local_directory
@@ -76,8 +84,9 @@ steps=[numpy.timedelta64(firststep,'h'), numpy.timedelta64(laststep,'h')]
 
 logger.info("ECMWFscrape starting...")
 
-if (args.scrapedryrun==0):
-	logger.info('Retrieving data from ECMWF')
+#This must be a wrong way to test if this field is blank
+if (args.grib2 !=''): 
+	logger.info('Scraping data from ECMWF')
 	client = Client()
 	result  = client.retrieve(
 		step=[firststep,laststep],
@@ -85,14 +94,15 @@ if (args.scrapedryrun==0):
 		param = ["2t","msl"],
 		date = args.date,
 		time = args.time,
-		target="data.grib2",
+		target=args.grib2_filename,
 	)
+	grib2file = args.grib2_filename
+else:
+	grib2file = args.grib2
 
-logger.info("Opening data from ECMWF")
 
-#Todo: Ideally we log the output of this command
-
-ds = xr.open_dataset('data.grib2',engine='cfgrib')
+logger.info("Opening grib2 file")
+ds = xr.open_dataset(grib2file,engine='cfgrib')
 
 #
 # Find the ulam points
@@ -101,16 +111,15 @@ ds = xr.open_dataset('data.grib2',engine='cfgrib')
 logger.info('Finding ulampoints')
 N=args.N
 start = time.time()
-ulamlist = findulam.compute_ulampoints_between_timesteps(ds,steps=steps,N=N,tolerance=tolerance)
+ulamlist = findulam.compute_ulampoints_between_timesteps(ds,steps=steps,N=N,tolerance=args.tolerance)
 logger.info('Finding ulampoints completed in '+str(time.time()-start)+'s')
-
 
 
 # Create a javascript file that contains the required data
 # All we record here is the ulamstep (rounded to second) and the ulampoint
 # Also we throw away those ulampoints not within a tolerances of 1e-8
 
-ulamlist_cropped = [[numpy.timedelta64(ulam['ulamstep'], 's').astype(float),ulam['ulampoint']] for ulam in ulamlist if ulam['OptimizeResult'].fun<tolerance]
+ulamlist_cropped = [[numpy.timedelta64(ulam['ulamstep'], 's').astype(float),ulam['ulampoint']] for ulam in ulamlist if ulam['OptimizeResult'].fun<args.tolerance]
 logger.info('ulamlist length: '+ str(len(ulamlist))+ ' of which '+ str(len(ulamlist_cropped))+ ' were found within tolerance')
 
 bu_filename = 'bu-'+numpy.datetime_as_string(ds.time.data,'D')+'T'+ str(args.time)+'h:'+str(firststep)+':'+str(laststep)+'.js'
@@ -138,6 +147,8 @@ logging.info('Writing bu.js file')
 f.write('var bu=' + json.dumps(bu)+'\n')
 f.close
 
+
+
 bu_ulampoints = {
 'ds_datetime':  numpy.datetime_as_string(numpy.datetime64(ds.time.data, 'ms')), 
 'ulamlist': ulamlist_cropped,
@@ -149,11 +160,9 @@ logging.info('Writing bu.js file')
 f.write('var bu=' + json.dumps(bu_ulampoints)+'\n')
 f.close
 
-s3bufilesdirectory = "s3://bursk-ulam-bufiles/"
-s3bufilesdirectory_url = "https://bursk-ulam-bufiles.s3.us-east-2.amazonaws.com/"
 
 #
-# Create a smaller javascript file that points to the larger bu data file 
+# Create a smaller javascript file that points to the larger bu data file (to be deprecated)
 #
 
 bu_datafile = bu_local_directory+'bu-latest-data.js'
@@ -161,43 +170,41 @@ with open(bu_datafile, 'w') as file:
 	file.write('var bulatestdatafilename="'+bu_filename+'.gz"')
 	
 #
-# Create a smaller javascript file that points to the larger bu data file (full url)
+# Create a smaller javascript file that points to the larger bu data file
 #
 
 bu_datafile_url = bu_local_directory+'bu-latest-data-url.js'
 with open(bu_datafile_url, 'w') as file:
-	file.write('var bulatestdataurl="'+s3bufilesdirectory_url+bu_filename+'.gz"')
+	file.write('var bulatestdataurl="'+args.bufile_url+bu_filename+'.gz"')
 	
 #
 # Move the files to the S3 buckets
 #
 if (args.s3dryrun==0):
-	# Copy the smaller javascript file to S3 bucket
+	# Copy the old bu pointer file to S3 bucket (hardcoded to be deprecated)
 	s3websitedirectory = "s3://julius-ross.com/Borsuk-Ulam/"
 	logger.info('Writing bu_datafile '+str(bu_datafile)+' to S3 bucket '+str(s3websitedirectory))
 	subprocessoutput=subprocess.run(["aws s3 cp "+bu_datafile+' '+s3websitedirectory+'bu-latest-data.js'], shell=True)
 	logger.info(subprocessoutput)
 	
-	# Copy the smaller javascript file to S3 bucket (full url)
-	s3websitedirectory_fullurl = "s3://bursk-ulam-pointer/"
-	logger.info('Writing bu_datafile full url '+str(bu_datafile_url)+' to S3 bucket '+str(s3websitedirectory_fullurl))
-	subprocessoutput=subprocess.run(["aws s3 cp "+bu_datafile_url+' '+s3websitedirectory_fullurl+'bu-latest-data-pointer.js'], shell=True)
+	# Copy the bu pointer file to S3 bucket
+	logger.info('Writing bu_datafile full url '+str(bu_datafile_url)+' to S3 bucket '+str(args.bupointer_s3bucket))
+	subprocessoutput=subprocess.run(["aws s3 cp "+bu_datafile_url+' '+args.bupointer_s3bucket+'bu-latest-data-pointer.js'], shell=True)
 	logger.info(subprocessoutput)
 
-	# Compress the data file
-	# TodoL Move this out of the s3 bucket dryrun command
+	# Compress the bu data file
 	logger.info('Compressing '+str(bu_local_filename))
 	subprocessoutput=subprocess.run(['gzip -f '+bu_local_filename], shell=True)
 	logger.info(subprocessoutput)
 	
-	# Copy the gzipped javascript data file to S3 bucket
-	logger.info('Copying zipped bu file '+str(bu_local_filename)+' to S3 bucket '+str(s3bufilesdirectory))
-	subprocessoutput=subprocess.run(["aws s3 cp "+bu_local_filename+'.gz'+' '+s3bufilesdirectory+' --content-encoding gzip'], shell=True)
+	# Copy the gzipped javascript data file to bu datafiles S3 bucket
+	logger.info('Copying zipped bu file '+str(bu_local_filename)+' to S3 bucket '+str(args.bufile_s3bucket))
+	subprocessoutput=subprocess.run(["aws s3 cp "+bu_local_filename+'.gz'+' '+args.bufile_s3bucket+' --content-encoding gzip'], shell=True)
 	logger.info(subprocessoutput)
 	
-	# Copy the ulampoints file to the S3 bucket
-	logger.info('Copying bu-ulampoints file '+str(bu_ulampoints_local_filename)+' to S3 bucket '+str(s3bufilesdirectory))
-	subprocessoutput=subprocess.run(["aws s3 cp "+bu_ulampoints_local_filename+' '+s3bufilesdirectory], shell=True)
+	# Copy the ulampoints file to the bu datafiles S3 bucket
+	logger.info('Copying bu-ulampoints file '+str(bu_ulampoints_local_filename)+' to S3 bucket '+str(args.bufile_s3bucket))
+	subprocessoutput=subprocess.run(["aws s3 cp "+bu_ulampoints_local_filename+' '+args.bufile_s3bucket], shell=True)
 	logger.info(subprocessoutput)
 
 logger.info('ECMWF script finished')
